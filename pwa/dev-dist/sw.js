60f4403f2c07abae5353d4dcdc5abfc794a510c2
sheikhhossainn <skhossain799@gmail.com>
2026-07-02 22:07:05 +0600

    feat: add service worker for offline support and asset precaching

--- pwa/dev-dist/sw.js
+++ pwa/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.02g6tl4757o"
+    "revision": "0.l543p63riq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
